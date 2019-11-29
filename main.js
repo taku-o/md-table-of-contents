e70e14e142c206ccda21e0bf3dcbabbe22c8fba0
taku.omi <taku.omi@gree.co.jp>
2019-11-29 16:13:10 +0900

    update action.

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 'use strict';
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { Toolkit } = require('actions-toolkit');
+const tools = new Toolkit();
 
 const toc = require('markdown-toc');
 const fs = require('fs');
@@ -8,11 +10,17 @@ const fs = require('fs');
 const mdFilePath = core.getInput('markdown-file-path');
 console.log(`target markdown file is ${mdFilePath}.`);
 
-    const contents = fs.readFileSync(mdFilePath);
-    const replaced = toc(contents).insert();
-    fs.writeFileSync(mdFilePath, replaced);
+const contents = fs.readFileSync(mdFilePath);
+const replaced = toc(contents).insert();
+fs.writeFileSync(mdFilePath, replaced);
 
-// commit
-// push
-// success
+const newCommits = await tools.github.createCommit({
+  owner: tools.context.owner,
+  repo: tools.context.repo,
+  message: 'update table of contents'
+  tree:
+  parents:
+});
+
+
 
